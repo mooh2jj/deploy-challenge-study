c17e840bbd9d8a5da988cbfe938f2d44f2c891b0
seonggon do <mooh2jj@naver.com>
2023-10-14 23:07:26 +0900

    2nd 원복

---
+++ 김소정/learnDocker/src/main/java/com/example/learndocker/LearnDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.learndocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDockerApplication.class, args);
+	}
+
+}
