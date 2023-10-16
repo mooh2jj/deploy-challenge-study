71bdf453ebd322515d77d50491fee64f4e2b24d8
minbo2002 <minbo2002@naver.com>
2023-10-17 02:43:28 +0900

    feat: 배포 첫 환경설정

---
+++ 김민보/week 1/src/test/java/com/example/deploy1week/Deploy1weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploy1week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy1weekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
